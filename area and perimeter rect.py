dd871472c793e087a8c35bc432d814e478682260
tanya987389 <t.anya987389@gmail.com>
2025-10-27 20:15:51 +0530

    Add files via upload

---
+++ area and perimeter rect.py
@@ -0,0 +1,8 @@
+length = float(input("enter the length of rectangle: "))
+breath = float(input("enter the breath of rectangle: "))
+
+area = length*breath
+perimeter = 2*(length+breath)               
+
+print("area of the rectangle: ",area)
+print("perimeter of the rectangle: ",perimeter)
