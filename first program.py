502584852d611d17098d09f12a380a130b7ee136
tanya987389 <t.anya987389@gmail.com>
2025-10-28 07:12:12 +0530

    Add files via upload

---
+++ first program.py
@@ -0,0 +1,12 @@
+num1 =int(input ("enter the first program:  "))
+num2 =int(input("enter the second program: "))
+
+sum_result =num1+num2
+difference_result =num1-num2
+product_result =num1*num2
+quoitient_result =num1/num2
+
+print("sum: ",sum_result)
+print("difference: ",difference_result)
+print("product:",product_result)
+print("quotient: ",quoitient_result)
