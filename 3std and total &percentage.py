c9b27c4550b2499de977d21c8c3e03aaf5af3eaa
tanya987389 <t.anya987389@gmail.com>
2025-10-28 07:11:16 +0530

    Add files via upload

---
+++ 3std and total &percentage.py
@@ -0,0 +1,22 @@
+#student
+print("enter marks of student1: ")
+s1_sub1 =int(input("subject1: "))
+s1_sub2 =int(input("subject2: "))
+s1_sub3 =int(input("subject3: "))
+s1_sub4 =int(input("subject4: "))
+s1_sub5 =int(input("subject5: "))
+s1_total =s1_sub1+s1_sub2+s1_sub3+s1_sub4+s1_sub5
+s1_percentage=(s1_total/500)*100
+
+#student
+print("enter marks of student2: ")
+s2_sub1 =int(input("subject1: "))
+s2_sub2 =int(input("subject2: "))
+s2_sub3 =int(input("subject3: "))
+s2_sub4 =int(input("subject4: "))
+s2_sub5 =int(input("subject5: "))
+s1_total =s2_sub1+s2_sub2+s2_sub3+s2_sub4+s2_sub5
+s2_percentage =(s2_total/500)*100
+
+#student
+s1_sub1 =int(input("subject1: "))
