e180a7e2d9abd90142097c408a81271581458c1f
tanya987389 <t.anya987389@gmail.com>
2025-10-28 07:13:41 +0530

    Add files via upload

---
+++ pallindrome.py
@@ -0,0 +1,8 @@
+string=  input ("enter a string :")
+
+string=string.lower()
+if string==string[ ::-1]:
+    print("the string is a palindrome.")
+else:
+    print("the string is not a palindrome.")
+    
